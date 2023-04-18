9bfe55fcb390e47f055d22c141ba807d7e61b3b1
safwendev <safwen.mastouri@gmail.com>
2023-04-18 04:51:18 +0100

    some changes for product details page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,50 +3,71 @@ import { useLocation } from "react-router-dom";
 import ProuctCard from "../component/ProuctCard";
 import "../style/detailsPage.css";
 import ReactStars from "react-rating-stars-component";
+import NavBar from "../component/NavBar";
 
 export default function ProductDetails() {
   const location = useLocation();
   const state = location.state;
   const { source, title, description, val, price } = state;
   console.log(description);
-  const [quantity, setQuantity] = useState(0);
-
+  const [quantity, setQuantity] = useState(1);
+  const [totalprice, setTotalPrice] = useState(price);
   const handleIncrement = () => {
     setQuantity(quantity + 1);
+    setTotalPrice(totalprice + price);
   };
 
   const handleDecrement = () => {
-    if (quantity >= 1) {
+    if (quantity > 1) {
       setQuantity(quantity - 1);
+      setTotalPrice(totalprice - price);
+    }
+    else if (quantity == 1) {
+      setTotalPrice(price)
     }
   };
 
   return (
-    <div className="details-container">
-      <div>
-        <img id="image" src={source} />
+    <>
+      <NavBar/>
+      <h3>Product details</h3>
+      <div className="details-container">
+        <div>
+          <img id="image" src={source} />
+        </div>
+        <div className="details">
+          <h3 id="product-title"> {title} </h3>
+          <p id="price"> {price}dt </p>
+          <p id="description"> {description} </p>
+          <ReactStars
+            size={24}
+            value={val}
+            activeColor="#ffd700"
+            edit={false}
+          />
+          <form>
+            <div
+              class="value-button"
+              id="decrease"
+              onClick={() => handleDecrement()}>
+              -
+            </div>
+            <input type="number" id="number" value={quantity} />
+            <div
+              className="value-button"
+              id="increase"
+              onClick={() => handleIncrement()}>
+              +
+            </div>
+            <div>
+              <p>total price</p>
+              { totalprice } dt
+              <p>delivery fees</p>
+              {7.9}
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="details">
-        <h3 id="product-title"> {title} </h3>
-        <p id="price"> {price}dt </p>
-        <p id="description"> {description} </p>
-        <ReactStars size={24} value={val} activeColor="#ffd700" edit={false} />
-        <form>
-          <div
-            class="value-button"
-            id="decrease"
-            onClick={() => handleDecrement()}>
-            -
-          </div>
-          <input type="number" id="number" value={quantity} />
-          <div
-            className="value-button"
-            id="increase"
-            onClick={() => handleIncrement()}>
-            +
-          </div>
-        </form>
-      </div>
-    </div>
+    </>
   );
 }
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useState, memo, useEffect } from "react";
+import React, { useState, memo, useMemo  } from "react";
 import NavBar from "../component/NavBar";
 import { food, drink } from "../data/data";
 import ProuctCard from "../component/ProuctCard";
@@ -8,6 +8,16 @@ import { useLocation } from "react-router-dom";
 
 function ProductPage() {
   const [empty, setEmpty] = useState(false);
+  const isObjectEmpty = (objectName) => {
+    return (
+      objectName &&
+      Object.keys(objectName).length === 0 &&
+      objectName.constructor === Object
+    );
+  };
+
+    
+
   const location = useLocation();
   const state = location.state;
   const condition = state === "food";
@@ -22,52 +32,60 @@ function ProductPage() {
   return (
     <>
       <NavBar />
-      {condition ? (
-        <h3 className="menu-titile">Food menu</h3>
-      ) : (
-        <h3 className="menu-titile">Drinks Menu</h3>
-      )}
-      <div className="box">
-        <input
-          type="text"
-          className="input"
-          name="txt"
-          value={searchTerm}
-          placeholder="Search ..."
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
-        />
-      </div>
-      <div className="product-container">
-        {data
-          .filter((item) => {
-            if (searchTerm == "") {
-              return item;
-            } else if (
-              item.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-            ) {
-              return item;
-            } else if (!item.length) {
-              setEmpty(!empty);
-            }
-          })
-          .map((item, key) => {
-            return (
-              <ProuctCard
-                id={ key }
-                key={key}
-                source={item.source}
-                title={item.title}
-                description={item.description}
-                val={item.val}
-                price={item.price}
-              />
-            );
-          })}
-      </div>
-      {empty && <p>dd</p>}
-      {console.log(empty)}
+      <main>
+        {condition ? (
+          <h3 className="menu-titile">Food menu</h3>
+        ) : (
+          <h3 className="menu-titile">Drinks Menu</h3>
+        )}
+        <div className="box">
+          <input
+            type="text"
+            className="input"
+            name="txt"
+            value={searchTerm}
+            placeholder="Search ..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+        </div>
+        <div className="product-container">
+          {data
+            .filter((item) => {
+              if (searchTerm == "") {
+                return item;
+              } else if (
+                item.title
+                  .toLowerCase()
+                  .includes(searchTerm.toLocaleLowerCase())
+              ) {
+                return item;
+              } else if (item == undefined || item == null) {
+                console.log(item);
+                let d = document.getElementsByTagName("p");
+                d[0].innerHTML = "dd";
+              }
+              {
+                console.log(item, item.key);
+              }
+            })
+            .map((item, key) => {
+              return (
+                <ProuctCard
+                  id={key}
+                  key={key}
+                  source={item.source}
+                  title={item.title}
+                  description={item.description}
+                  val={item.val}
+                  price={item.price}
+                />
+              );
+            })}
+        </div>
+        <p></p>
+      </main>
       <Footer />
     </>
   );
